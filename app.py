2ec2f669a052583e657bd6f7282faa025f535236
IsaiahSama <zeldaplayergl@gmail.com>
2021-12-28 19:57:53 -0400

    Added route to handle uploading g of a file to the server

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'db/'
+ALLOWED_EXTENSIONS = ['sqlite3', 'txt']
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def hello_world():
@@ -23,3 +28,33 @@ def sayhello():
     except KeyError:
         return {"RESPONSE": "NO"}
     return {"RESPONSE": text}
+
+def allowed_file(filename:str):
+    """Determines whether an uploaded file is allowed or not
+    
+    Args:
+        filename (str): The name of the file to check
+        
+    Returns:
+        bool"""
+
+    return '.' in filename and filename.split('.')[-1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/takefile/", methods=["POST"])
+def takefile():
+    if 'file' not in request.files:
+        return {"ERROR": "No file was received"}
+    
+    file = request.files['file']
+    if file.filename == '':
+        return {"ERROR": "No given file"}
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        with open(f"{app.config['UPLOAD_FOLDER']}{filename}", "wb") as fp:
+            file.save(fp)
+
+        return {"RESPONSE": "File was successfully uploaded"}
+    else:
+        return {"ERROR": "Invalid file provided"}
